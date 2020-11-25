06d01ac24b71c2e03da448b366974ea779d0312a
Brayden England <bray.patriot@gmail.com>
2020-11-25 00:04:55 -0700

    Background component added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, {useState, useEffect} from 'react';
+import './reset.scss'
+import './App.scss'
 
 // figures out the contrast between two hex values
 import chroma from 'chroma-js' // https://vis4.net/chromajs/#quick-start
@@ -7,14 +9,23 @@ import chroma from 'chroma-js' // https://vis4.net/chromajs/#quick-start
 // apply the same process to obtain the color ratios and determine their grade according to the WCC (or whatever it's called)
 import blinder from 'color-blind' // https://github.com/skratchdot/color-blind
 
+// color picker
 import { SketchPicker } from 'react-color' // https://casesandberg.github.io/react-color/
 
+// COMPONENTS
+import Background from './components/Background/Background'
+
 const App = () => {
+
+  const [backgroundColor, setBackgroundColor] = useState('#DE6464')
+  const [textColor, setTextColor] = useState('#FFFFFF')
+
   let chromaColor = chroma('pink').darken().saturate(2).hex()
   let blindColor = blinder.protanopia('#42dead')
 
   return (
     <div className="App">
+      <Background color={backgroundColor} />
       Chroma Color: { chromaColor } Blind Color: { blindColor }
       <SketchPicker />
     </div>
---
+++ src/components/Background/Background.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './background.scss'
+
+const Background = (props) => {
+    return (
+        <div className="Background" style={{backgroundColor: props.color}}>
+            
+        </div>
+    )
+}
+
+export default Background
