716c596954b28a4822b4df13956a0b0d464e2efe
Brayden England <bray.patriot@gmail.com>
2020-11-24 23:32:17 -0700

    Packages installed and implemented

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
 
-function App() {
+// figures out the contrast between two hex values
+import chroma from 'chroma-js' // https://vis4.net/chromajs/#quick-start
+
+// converts hex codes into ones that would be seen by people with the different impairments; 
+// apply the same process to obtain the color ratios and determine their grade according to the WCC (or whatever it's called)
+import blinder from 'color-blind' // https://github.com/skratchdot/color-blind
+
+import { SketchPicker } from 'react-color' // https://casesandberg.github.io/react-color/
+
+const App = () => {
+  let chromaColor = chroma('pink').darken().saturate(2).hex()
+  let blindColor = blinder.protanopia('#42dead')
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Chroma Color: { chromaColor } Blind Color: { blindColor }
+      <SketchPicker />
     </div>
   );
 }
